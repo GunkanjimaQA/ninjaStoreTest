12aff1b069d9e17d0fa09e71b9f227e5408bf03f
GunkanjimaQA <47102954+GunkanjimaQA@users.noreply.github.com>
2020-01-17 02:52:12 +0200

    Code fix

--- src/main/java/pages/AccountPage.java
+++ src/main/java/pages/AccountPage.java
@@ -4,8 +4,6 @@ import libs.Utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import parentpage.ParentPage;
 
 import java.util.List;
